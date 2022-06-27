a2c6cc4e961e8b85f648d18efba16048e729edf5
JamesLiuZX <liu.zixin@dhs.sg>
2022-06-27 20:08:44 +0800

    Sentiment png

--- frontend/src/components/Dashboards/Sentiment/index.tsx
+++ frontend/src/components/Dashboards/Sentiment/index.tsx
@@ -2,11 +2,19 @@ import { FunctionalComponent, h } from "preact";
 
 const Sentiment: FunctionalComponent = () => {
     return (
+        <header class={style.header}>
         <div>
             <h1>
                 This is the Sentiment Component.
             </h1>
+            <a href="/">
+                <h1>
+                    <img class={style.logo} src="../../assets/sentiment.png"></img>
+                    DogeTTM<sup>®</sup>
+                </h1>
+            </a>
         </div>
+        </header>
     );
 }
 
